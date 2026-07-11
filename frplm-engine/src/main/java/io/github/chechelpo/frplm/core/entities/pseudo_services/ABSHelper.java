4a856b855aa9d60f4e55eca098c97aa553ad1457
chechelpo <felipeyelpo26@gmail.com>
2026-07-10 23:39:08 -0300

    Feat: new FieldBuilder class and register_field(column) that creates it. More ergonomic

--- frplm-engine/src/main/java/io/github/chechelpo/frplm/core/entities/pseudo_services/ABSHelper.java
+++ frplm-engine/src/main/java/io/github/chechelpo/frplm/core/entities/pseudo_services/ABSHelper.java
@@ -5,6 +5,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jooq.TableField;
 import org.jooq.TableRecord;
 
+import java.util.Objects;
+
 public abstract class ABSHelper<
         R extends TableRecord<R>,
         Service extends EntityService<R,?>
@@ -25,6 +27,8 @@ public abstract class ABSHelper<
             @NotNull TableField<R, T> column,
             @NotNull FieldInfo<?> info
     ) {
+        Objects.requireNonNull(column, "Column field is null");
+        Objects.requireNonNull(info, "Field info is null");
         service.registerField(column, info.require, info.constraints);
     }
     protected <T> void register_field(
@@ -32,6 +36,38 @@ public abstract class ABSHelper<
             @NotNull FieldInfo<?> info,
             T defaultValue
     ){
+        if (info.constraints.validateConstraint(column, defaultValue, false).isPresent())
+            throw new IllegalArgumentException("Default value does not pass constraint");
+
         service.registerField(column, info.require, info.constraints, defaultValue);
+    }
+
+    protected <T> void register_field(FieldBuilder<T> builder){
+        Objects.requireNonNull(builder, "Builder is null");
+        this.register_field(builder.column, builder.info, builder.defaultValue);
+    }
+
+    public <T> FieldBuilder<T> register_field(TableField<R, T> column){
+        return new FieldBuilder<>(column);
+    }
+
+    public class FieldBuilder<T> {
+        private final TableField<R,T> column;
+        FieldInfo<?> info = null;
+        T defaultValue = null;
+
+        private FieldBuilder(TableField<R,T> column){
+            this.column = column;
+        }
+
+        public FieldBuilder<T> setInfo(FieldInfo<?> info){
+            this.info = info;
+            return this;
+        }
+
+        public FieldBuilder<T> withDefaultValue(T defaultValue){
+            this.defaultValue = defaultValue;
+            return this;
+        }
     }
 }
